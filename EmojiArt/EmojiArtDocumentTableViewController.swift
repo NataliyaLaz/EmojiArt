1728b5f2b6198000a1f91b617911d5c2b1e649cc
Nataliya <nataliya.laz@gmail.com>
2022-08-30 10:12:47 +0300

    add smith

--- EmojiArt/EmojiArtDocumentTableViewController.swift
+++ EmojiArt/EmojiArtDocumentTableViewController.swift
@@ -44,7 +44,7 @@ class EmojiArtDocumentTableViewController: UITableViewController {
     */
 
     
-    // Override to support editing the table view.
+   
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
